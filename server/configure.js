5ba37d4ae758a179e46c1364b187649b0611804b
Jean <jeanharmsen@yahoo.com>
2018-11-02 15:41:52 +0000

    set up handlebars as the template engine in configure.js

--- server/configure.js
+++ server/configure.js
@@ -1,10 +1,19 @@
 /* Pull in the dependencies */
 var routes = require('./routes'),
-    express = require('express');
+    express = require('express'),
+    exphbs = require('express-handlebars');
 
 module.exports = function(app){
 	/* Put app into routes constructor */
         routes(app);
+
+        /* Set up handlebars as the template engine */
+        app.engine('handlebars', exphbs.create({
+		defaultLayout: 'main',
+		layoutsDir: app.get('views') + '/layouts',
+ 		partialsDir: app.get('views') + '/partials',
+	}).engine);
+      	app.set('view engine', 'handlebars');
 
         /* Return app */
         return app;
